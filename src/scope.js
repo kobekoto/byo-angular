b126f69da2070d2b8d44754c991e72855eb1695c
Kobe Osei-Akoto <kobe.oseiakoto@gmail.com>
2016-08-27 23:21:00 -0400

    init implementation of watch and digest

--- src/scope.js
+++ src/scope.js
@@ -1,7 +1,28 @@
 'use strict';
 
-function Scope() {
+var _ = require('lodash');
 
+function Scope() {
+    // Giving the scope a place to store all watchers that have been registered.
+    // $$ before watchers means that $$watchers should be considered private 
+    // to the Angular framework and not be called from the app code.
+    this.$$watchers = [];
 }
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+    var watcher = {
+        watchFn: watchFn,
+        listenerFn: listenerFn
+    };
+    // Each $watch pushes the watch and listenerFn to the array of $$watchers
+    this.$$watchers.push(watcher);
+};
+
+//Loops over each watcher and calls the listenerFn
+Scope.prototype.$digest = function() {
+    _.forEach(this.$$watchers, function(watcher) {
+        watcher.listenerFn();
+    });
+};
 
 module.exports = Scope;
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -13,4 +13,32 @@ describe('Scope', function() {
         expect(scope.aProperty).toBe(1);
     });
 
+    describe('digest', function() {
+
+        var scope;
+
+        // initializes a new scope before each test
+        beforeEach(function() {
+            scope = new Scope();
+        });
+
+        it('calls the listener function of a watch on first $digest', function() {
+            // For now watchFn is simply returning a string
+            var watchFn = function() {
+                return 'wat';
+            };
+            // jasmine.createSpy creates a callable function (a mock function)
+            var listenerFn = jasmine.createSpy();
+
+            scope.$watch(watchFn, listenerFn);
+
+
+            scope.$digest();
+            // After we call scope.$digest we're expecting to check that the 
+            // listenerFn was actually called
+            expect(listenerFn).toHaveBeenCalled();
+        });
+
+    });
+
 });
